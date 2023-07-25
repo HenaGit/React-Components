e6b7b570a3dba3781e266567650f2e25f0000e9f
HenokG <heni.gebrehiwot@gmail.com>
2023-07-25 16:11:43 -0600

    Section 2 - First React Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+REACT COURSE
+In this course, we will learn react js by building TaskOPedia! 
+    •	Call Ben
+    •	Go to walmart
+Happy Coding!
+*/
+function MainBody() {
+  return (
+    <div>
+      <p>In this course, we will learn react js by building TaskOPedia!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to walmart</li>
+      </ul>
+    </div>
+  );
+}
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <div>
+    <MainBody />
+    <MainBody />
+    <MainBody />
+  </div>
 );
 
 
