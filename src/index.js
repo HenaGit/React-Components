b93dcc60e27eed1df4aa8b911df2ea9fc798437e
HenokG <heni.gebrehiwot@gmail.com>
2023-07-25 17:50:53 -0600

    Section 3 - HTML Element in JSX

--- src/index.js
+++ src/index.js
@@ -22,13 +22,20 @@ function Header() {
 }
 function MainBody() {
   const whatWeWillLearn = "React JS";
+  const lectureCount = 3;
   return (
     <div>
-      <p>In this course, we will learn {whatWeWillLearn} by building TaskOPedia!</p>
+      In this course, we will learn {whatWeWillLearn} by building TaskOPedia!{" "}
+        <br />
+        Total Lecture - {lectureCount}
       <ul>
         <li>Basic Foundation</li>
         <li>Functional and Class Components</li>
       </ul>
+     {/*  <div>
+        Enter Task :{" "}
+        <input maxLength={5} readOnly={false} placeholder="Henok"></input>
+      </div> */}
     </div>
   );
 }
