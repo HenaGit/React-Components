8408f98a0effbbc65977d2590731e1254b50a64e
HenokG <heni.gebrehiwot@gmail.com>
2023-07-25 18:18:40 -0600

    Section 3 - Seperating Student Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import "./CSS/style.css";
 import Header from "./header";
+import Students from './Students';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
@@ -23,28 +24,7 @@ function MainBody() {
     </div>
   );
 }
-function Students() {
-  const fullName = "Henok Gebrehiwot";
-  const programmingExp = 11;
-  return (
-    <div className="container p-4">
-      <div className="row">Students Enrolled</div>
-      <div className="row border">
-        <div className="col-2">
-          <img
-            src={`https://ui-avatars.com/api/?name=${fullName}`}
-            className="w-100" alt='Henk'
-          ></img>
-        </div>
-        <div className="col-10">
-          {fullName}
-          <br />
-          Coding Experience {programmingExp} years
-        </div>
-      </div>
-    </div>
-  );
-}
+
 function Footer() {
   return <p style={{ color: "gray", backgroundColor: "black" }}>Happy Coding!</p>;
 }
